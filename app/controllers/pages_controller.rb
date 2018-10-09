77eaf577df4d8797300e5acdf67ba261003269ab
chris held <chris.held@zyquest.com>
2018-10-09 14:23:10 -0500

    messed with tests a little, refactored main posts query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,27 @@
 class PagesController < ApplicationController
+
   def index
-    @posts = Post.select("id, name, created_at, category").order("created_at desc").limit(5)
+    @posts = Post.select(fields()).order("created_at desc").limit(5)
   end
 
   def coding
-    @posts = Post.coding.select("id, name, created_at, category").order("created_at desc").limit(5)
+    @posts = Post.coding.select(fields()).order("created_at desc").limit(5)
   end
 
   def cooking
-    @posts = Post.cooking.select("id, name, created_at, category").order("created_at desc").limit(5)
+    @posts = Post.cooking.select(fields()).order("created_at desc").limit(5)
   end
 
   def wtf
-    @posts = Post.wtf.select("id, name, created_at, category").order("created_at desc").limit(5)
+    @posts = Post.wtf.select(fields()).order("created_at desc").limit(5)
   end
 
   def archive
-    @posts = Post.select("id, name, created_at, category").order("created_at desc").all
+    @posts = Post.select(fields()).order("created_at desc").all
   end
+
+  private
+    def fields
+      "id, name, created_at, category"
+    end
 end
